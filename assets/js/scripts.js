7c9fef4802ee7304ade4a873c9417878216ad9da
Jean Guedes <jean_guedes_sa@hotmail.com>
2021-06-27 20:20:32 -0700

    Adding my changes

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+/* Here's an example script! You cna include anything you'd like to include
+on your website in here! Uncomment (remove the //) the scripts to have it run! */
+
+//alert("Cullen is bringing me food tonight.")
